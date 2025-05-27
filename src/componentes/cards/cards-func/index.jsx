680c4d3d7868138b6101bf5edeac9ee373c05ff7
BEaew0 <beatrizsouzasscoli@gmail.com>
2025-05-27 11:17:23 -0300

    me ajuda

--- src/componentes/cards/cards-func/index.jsx
+++ src/componentes/cards/cards-func/index.jsx
@@ -1,6 +1,6 @@
 import "./card.css";
 
-export default function Card({ cinza, titulo, texto }) {
+export default function Card({ cinza, titulo, texto , button }) {
     return (
         <div className={`card-inicial ${cinza ? "card-cinza" : "card-azul"}`}>
             <h1>{titulo}</h1>
